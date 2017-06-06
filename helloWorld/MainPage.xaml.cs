b17264de75784dbc6c304ca75cb40c9e4f714b06
CaptainSundae <nikisiasoco@gmail.com>
2017-06-06 18:44:51 +1200

    Add project files.

---
+++ helloWorld/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace helloWorld
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBlock.Text = "Hello World. We're done presenting.";
+            ImageSource imagesource = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("http://s2.quickmeme.com/img/91/91dd85156980eb1e4de4c8a830faf1b6a3943afcdb5ce77b831c6388eeb6b98a.jpg"));
+            MemeBlock.Source = imagesource;
+            
+        }
+    }
+}
